737cc0718a6aaf99388a281a6de7798695ac3e2c
DSE <shahbaz.akhter9823@gmail.com>
2025-11-13 13:42:37 +0530

    Define these options once at the top of the file

--- ingestion-api/src/services/loggerService.js
+++ ingestion-api/src/services/loggerService.js
@@ -1,5 +1,15 @@
 import { loggerQueue, debugQueue } from '../../../shared/config/bullmq.js';
-
+// --- DEFINE YOUR CLEANUP OPTIONS ---
+// Define these options once at the top of the file
+const jobOptions = {
+  removeOnComplete: {
+    count: 1000, // Keep the last 1000 completed jobs
+    age: 60 * 60 * 24, // Keep jobs for a max of 24 hours
+  },
+  removeOnFail: {
+    count: 1000, // Keep the last 5000 failed jobs
+  },
+};
 function parseMessages(raw) {
   const trimmedRaw = raw.trim();
   if (trimmedRaw.startsWith('[')) return JSON.parse(trimmedRaw);
@@ -15,7 +25,7 @@ async function processPacket(msg) {
   const sid = modbus?.[0]?.sid;
 
   if (msgType === 'login' || msgType === 'sys') {
-    await debugQueue.add('system-packets', { packet: msg });
+    await debugQueue.add('system-packets', { packet: msg } ,jobOptions);
     return { success: true, type: 'system' };
   }
 
@@ -32,7 +42,7 @@ async function processPacket(msg) {
     await loggerQueue.add(
       'logger-packets',
       { packet: msg },
-      { jobId: `${imei}_${sid}_${dtm || time}`, removeOnComplete: true , removeOnFail: true }
+      { jobId: `${imei}_${sid}_${dtm || time}`, jobOptions }
       
     );
     return { success: true, type: 'data' };
